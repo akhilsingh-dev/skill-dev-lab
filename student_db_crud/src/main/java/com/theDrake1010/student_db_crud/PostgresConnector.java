942f7ed82571656a3f431c61169b1028eb153e05
theDrake1010 <40403909+theDrake1010@users.noreply.github.com>
2021-02-04 20:03:12 +0530

    added record listing and insertion

--- student_db_crud/src/main/java/com/theDrake1010/student_db_crud/PostgresConnector.java
+++ student_db_crud/src/main/java/com/theDrake1010/student_db_crud/PostgresConnector.java
@@ -2,6 +2,7 @@ package com.theDrake1010.student_db_crud;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,13 +10,7 @@ public class PostgresConnector {
     private final String db_name = "test";
     private final String db_url = "jdbc:postgresql://localhost/" + db_name;
     private final String user = "postgres";
-    private final String password = "<ENTER PASSWORD>";
-
-    // private static final String createTableSQL = "CREATE TABLE IF NOT EXISTS students " +
-    //     "(ID INT PRIMARY KEY ," +
-    //     " NAME TEXT, " +
-    //     " EMAIL VARCHAR(50), " +
-    //     " COUNTRY VARCHAR(50), ";
+    private final String password = "<REDACTED>";
 
     public Connection connectToDB(){
         Connection conn = null;
@@ -28,10 +23,71 @@ public class PostgresConnector {
             sqle.printStackTrace();
         }
         catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return conn;
+    }
+
+    public void createTable(Connection conn, String tableName) {
+        Statement statement;
+        try {
+            String query = "CREATE TABLE IF NOT EXISTS " + tableName + 
+                "(prn SERIAL PRIMARY KEY," + 
+                " name VARCHAR(200)," +
+                " course VARCHAR(20)," +
+                " institute  VARCHAR(50)," +
+                " grade REAL);";
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("[INFO] : " + tableName + " Table Created!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertRow(Connection conn, String tableName, String name, String course, String institute, double grade) {
+        Statement statement;
+        try {
+            String query = String.format(
+                "INSERT INTO %s(" +
+                    "name,course,institute,grade"+
+                ") VALUES(" +
+                    "'%s','%s','%s',%f)",
+                tableName,name,course,institute,grade);
+            
+            
+            System.out.println(query);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("[INFO] : Student inserted succesfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+    public void getStudents(Connection conn, String tableName) {
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            String query = String.format("SELECT * FROM %s;", tableName);
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery(query);
+            System.out.println("_______________________________________________________________________________");
+            System.out.println("PRN\t\tName\t\tCourse\t\tInstitute\t\tGrade");
+            System.out.println("_______________________________________________________________________________");
+            
+            while(resultSet.next()){
+                System.out.print(resultSet.getInt("prn")+"\t\t");
+                System.out.print(resultSet.getString("name")+"\t\t");
+                System.out.print(resultSet.getString("course")+"\t\t");
+                System.out.print(resultSet.getString("institute")+"\t\t");
+                System.out.println(resultSet.getDouble("grade"));
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
